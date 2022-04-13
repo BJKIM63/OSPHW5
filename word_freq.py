abff7bbef969f7dc7b3a682c962b75134d913ce4
BJKIM63 <sgsb4027@gmail.com>
2022-04-13 16:44:14 +0000

    feat: split

--- word_freq.py
+++ word_freq.py
@@ -2,8 +2,12 @@
 import string
 fpin = open("article.txt", "r")
 
+words = list()
+
 while 1:
     line = fpin.readline()
     if not line:
         break
-    print(line)
+    words += line.split()
+
+print(words)
